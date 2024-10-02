9e7faa61fb6a60d4f80a6ba863bf9cefeeea1153
evankeane <145344743+evankeane@users.noreply.github.com>
2024-10-02 16:57:01 +0700

    Initial commit

---
+++ resources/views/buku/index.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <h1>ini Halaman Buku</h1>
+    <a href="buku/create">tambah data</a>
+</x-layout>
---
+++ resources/views/landingPage.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+
+
+    <h1>Ini Adalah Halaman Landing Page</h1>
+</x-layout>
